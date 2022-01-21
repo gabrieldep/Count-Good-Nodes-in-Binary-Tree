ac136e94340a8d975e8282b822433f47e313a260
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-01-21 20:51:17 -0300

    CodeStyle

--- CountGoodNodesinBinaryTree/CountGoodNodesinBinaryTree/Program.cs
+++ CountGoodNodesinBinaryTree/CountGoodNodesinBinaryTree/Program.cs
@@ -1,17 +1,4 @@
-﻿public class TreeNode
-{
-    public int val;
-    public TreeNode? left;
-    public TreeNode? right;
-    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-    {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
-public class Program
+﻿public class Program
 {
     public static void Main()
     {
@@ -58,5 +45,17 @@ public class Program
             return ++soma;
         }
         return soma;
+    }
+}
+public class TreeNode
+{
+    public int val;
+    public TreeNode? left;
+    public TreeNode? right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
