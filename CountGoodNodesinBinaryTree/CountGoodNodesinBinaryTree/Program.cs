089a65a1cb6ce752e2c21df748156edde582fecd
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-08-24 13:04:19 -0300

    Add: Namespace

--- CountGoodNodesinBinaryTree/CountGoodNodesinBinaryTree/Program.cs
+++ CountGoodNodesinBinaryTree/CountGoodNodesinBinaryTree/Program.cs
@@ -1,58 +1,59 @@
-﻿public class Program
+﻿namespace CountGoodNodes
 {
-    public static void Main()
+    public class Program
     {
-        TreeNode root3a = new(3, new(1, new(3)), new(4, new(1), new(5)));
-        Console.WriteLine("Teste 1- " + (GoodNodes(root3a) == 4 ? "Correto" : "Errado"));
-    }
+        public static void Main()
+        {
+            TreeNode root3a = new(3, new(1, new(3)), new(4, new(1), new(5)));
+            Console.WriteLine("Teste 1- " + (GoodNodes(root3a) == 4 ? "Correto" : "Errado"));
+        }
 
-    public static int GoodNodes(TreeNode root)
-    {
-        List<int> listCaminho = new()
+        public static int GoodNodes(TreeNode root)
+        {
+            var listCaminho = new List<int>()
         {
             root.val
         };
-        return IsGoodNode(root, listCaminho);
-    }
+            return IsGoodNode(root, listCaminho);
+        }
 
-    internal static int IsGoodNode(TreeNode root, List<int> list)
-    {
-        int soma = 0;
-        if (root.left != null)
+        internal static int IsGoodNode(TreeNode root, List<int> list)
         {
-            list.Add(root.left.val);
-            soma += IsGoodNode(root.left, list);
-            list.Remove(root.left.val);
-            root.left = null;
-        }
-        if (root.right != null)
-        {
-            list.Add(root.right.val);
-            soma += IsGoodNode(root.right, list);
-            list.Remove(root.right.val);
-            root.right = null;
-        }
-        if (root.left == null && root.right == null)
-        {
-            foreach (var item in list)
+            int soma = 0;
+            if (root.left != null)
             {
-                if (root.val < item)
-                    return soma;
+                list.Add(root.left.val);
+                soma += IsGoodNode(root.left, list);
+                list.Remove(root.left.val);
+                root.left = null;
             }
-            return ++soma;
+            if (root.right != null)
+            {
+                list.Add(root.right.val);
+                soma += IsGoodNode(root.right, list);
+                list.Remove(root.right.val);
+                root.right = null;
+            }
+            if (root.left == null && root.right == null)
+            {
+                foreach (var item in list)
+                    if (root.val < item)
+                        return soma;
+                return ++soma;
+            }
+            return soma;
         }
-        return soma;
     }
-}
-public class TreeNode
-{
-    public int val;
-    public TreeNode? left;
-    public TreeNode? right;
-    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    public class TreeNode
     {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+        public int val;
+        public TreeNode? left;
+        public TreeNode? right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }
